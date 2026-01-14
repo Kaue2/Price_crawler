6f864644fadf914634c37a70a608ce13c182a70c
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-14 15:07:00 -0300

    feat: definir item para trafegar pelo scrapy

--- crawlers/price_crawler/items.py
+++ crawlers/price_crawler/items.py
@@ -10,3 +10,9 @@ class PriceCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class RawHtmlItem(scrapy.Item): 
+    name = scrapy.Field()
+    url = scrapy.Field()
+    html = scrapy.Field()
+    createdAt = scrapy.Field()
