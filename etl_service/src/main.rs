ae0f1037bdcca5208a5bda5df6d64ff9ea14e9ca
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-19 13:18:05 -0300

    feat: implementar conexão com rabbit mq

--- etl_service/src/main.rs
+++ etl_service/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties};
+use futures_lite::stream::StreamExt;
+use std::env;
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let mq_user = match env::var("MQ_USER") {
+        Ok(value) => value,
+        Err(_) => return Err("Erro: falha ao buscar pelo usuário do RabbitMQ")  
+    }; 
+
+    let mq_password = match env::var("MQ_PASS") {
+        Ok(value) => value,
+        Err(_) => return Err("Erro: falha ao buscar pela senha do RabbitMQ")
+    };
+
+    let rabbit_addr = format!("amqp://{}:{}@localhost:5672/%2f", mq_user, mq_password);
+
+    let conn = Connection::connect(&rabbit_addr, ConnectionProperties::default()).await?;
+    let channel = conn.create_channel().await?;
+
+    Ok(())
 }
