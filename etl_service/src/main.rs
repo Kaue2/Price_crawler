3d446ec4ae536f9ed34b7ebd6baff4750bba81c0
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-20 18:59:13 -0300

    feat: implementar captura de preço no html

--- etl_service/src/main.rs
+++ etl_service/src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 use mongodb::{Client, bson::document, options::ClientOptions};
+use scraper::{Selector, Html};
 
 #[derive(Debug, Deserialize)]
 struct RabbitMessage {
@@ -19,10 +20,7 @@ struct RawPage {
     html: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-
+async fn connect_rabbit() -> Result<lapin::Channel, lapin::Error> {
     let mq_user = env::var("MQ_USER").expect("Erro: variável MQ_USER não encontrada");
     let mq_password = env::var("MQ_PASS").expect("Erro: variável MQ_PASS não encontrada");
     let rabbit_addr = format!("amqp://{}:{}@localhost:5672/%2f", mq_user, mq_password);
@@ -31,6 +29,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn = Connection::connect(&rabbit_addr, ConnectionProperties::default()).await?;
     let channel = conn.create_channel().await?;
 
+    Ok(channel)
+}
+
+async fn connect_mongo() -> Result<mongodb::Collection<RawPage>, mongodb::error::Error> {
     let mongo_user = env::var("MONGO_USER").expect("Erro: variável MONGO_USER não encontrada");
     let mongo_pass = env::var("MONGO_PASSWORD").expect("Erro: variável MONGO_PASS não encontrada");
     let mongo_uri = format!("mongodb://{}:{}@localhost:27017", mongo_user, mongo_pass);
@@ -41,6 +43,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db = mongo_client.database("crawler_db");
     let collection = db.collection::<RawPage>("raw_pages");
 
+    Ok(collection)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let channel = connect_rabbit().await?;
+    let collection = connect_mongo().await?;
+
     // criando uma stream (pipe para comunicação)
     let mut consumer = channel
         .basic_consume(
@@ -50,6 +62,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             FieldTable::default(),
         ).await?;
 
+    // loop para pegar mensagens
     while let Some(delivery) = consumer.next().await {
         if let Ok(delivery) = delivery {
             let payload: RabbitMessage = match serde_json::from_slice(&delivery.data){
@@ -68,6 +81,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             match collection.find_one(filter).await {
                 Ok(Some(document)) => {
                     println!("LOG: Documento encontrado. URL: {}", document.url);
+                    let fragment = Html::parse_fragment(&document.html);
+                    let selector = Selector::parse("p.price_color").unwrap();
+                    let price_element = fragment.select(&selector).next().unwrap();
+                    let price = price_element.inner_html();
+                    let price = price.replace("£", "");
+                    let price: f64 = price.parse().expect("ERROR: falha ao converter preço do item para número.");
+                    println!("DEBUG: preço encontrado: {}", price);
                 },
                 Ok(None) => eprintln!("WARNING: documento não encontrado no mongo, ID: {}", payload.id),
                 Err(e) => eprintln!("ERROR: falha de conexão com Mongo: {}", e),
