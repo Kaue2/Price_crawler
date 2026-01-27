3cf09a85be2d9803cc4d18cca0f258dedbb7d4c9
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-27 15:42:58 -0300

    refac: melhorar as tabelas

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,36 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS prices (
-    id UUID PRIMARY KEY,
-    title TEXT NOT NULL,
-    value DECIMAL NOT NULL,
-    url TEXT NOT NULL,
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE products (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    url TEXT UNIQUE NOT NULL,
+    title VARCHAR(255),
+    store VARCHAR(50),
+    last_checked_at TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE price_history ( 
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    value DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- wish list 
+CREATE TABLE user_products (
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    target_price DECIMAL (10, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, product_id)
+);
+
+CREATE INDEX id_price_history_product_id ON price_history(product_id);
+CREATE INDEX idx_price_history_date ON price_history(created_at DESC);
