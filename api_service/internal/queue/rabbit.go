a8f2271a8c5d57a2b09331dd6e469656bc9be342
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-28 19:24:59 -0300

    feat: criar módulo para conexão com RabbitMQ

---
+++ api_service/internal/queue/rabbit.go
@@ -0,0 +1,30 @@
+package queue
+
+import (
+	"fmt"
+	ampq "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQ struct {
+	conn *ampq.Connection
+	channel *ampq.Channel
+}
+
+func NewRabbitMQConnection(connString string) (*RabbitMQ, error){
+	conn, err := ampq.Dial(connString)
+	if err  != nil {
+		return nil, fmt.Errorf("ERROR: falha ao conectar com RabbitMQ: %w", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("ERROR: falha ao abrir canal RabbitMQ: %w", err)
+	}
+
+	rabbitMQClient := &RabbitMQ{
+		conn: conn,
+		channel: ch,
+	}
+
+	return rabbitMQClient, nil;
+}
