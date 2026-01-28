75a34f699e80fc0a4e5bd735e1e9845003f482ad
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-28 19:36:16 -0300

    feat: implementar primeira conexão com rabbitMQ

--- api_service/cmd/api/main.go
+++ api_service/cmd/api/main.go
@@ -4,27 +4,37 @@ import (
 	"log"
 	"os"
 	"price-crawler-api/internal/database"
+	"price-crawler-api/internal/queue"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	pwd, _ := os.Getwd()
-	log.Printf("WARNING: caminho para o .env: %s\n", pwd)
+	//pwd, _ := os.Getwd()
+	//log.Printf("WARNING: caminho para o .env: %s\n", pwd)
 
 	if err := godotenv.Load("../.env"); err != nil {
-		log.Println("WARNING: não foi possível carregar .env")
+		log.Fatalf("ERROR: não foi possível carregar .env")
 	}
 
 	postgre_conn := os.Getenv("DATABASE_URL")
-	log.Printf("Conexão com Postgre em: %s\n", postgre_conn)
+	rabbit_conn := os.Getenv("MQ_CONNECTION")
+	log.Printf("DEBUG: Conexão com Postgre em: %s\n", postgre_conn)
+	log.Printf("DEBUG: Conexão com Rabbit em: %s\n", rabbit_conn)
 
 	store, err := database.NewStore(postgre_conn)
 
 	if err != nil {
-		log.Printf("ERROR: falha ao conectar com Postgre: %s\n", err)
+		log.Fatalf("ERROR: falha ao conectar com Postgre: %s\n", err)
 	}
 	defer store.Close() // Agenda destruição para o final da função
 
+	rabbit, err := queue.NewRabbitMQConnection(rabbit_conn)
+	if err != nil {
+		log.Fatalf("ERROR: falha ao conectar com RabbitMQ: %s\n", err)
+	}
+	defer rabbit.Close()
+
 	log.Println("DEBUG: conexão com postgre estabelecida")
+	log.Println("DEBUG: conexão com rabbit estabelecida")
 }
