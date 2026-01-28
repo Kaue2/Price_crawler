ea3c02e565b7c94aeace6fdcd1dfdcce6f5b59e5
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-01-28 14:41:44 -0300

    style: mudar o nome da pasta para seguir o padrão do projeto

---
+++ scrap_service/price_crawler/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PriceCrawlerItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class RawHtmlItem(scrapy.Item): 
+    _id = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
+    html = scrapy.Field()
+    createdAt = scrapy.Field()
