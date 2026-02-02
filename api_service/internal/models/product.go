d7a79b981bd2bd93a8f8ad9fab011cd99968e46f
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-02 14:09:37 -0300

    refac: adicionar campo faltante ao modelo

--- api_service/internal/models/product.go
+++ api_service/internal/models/product.go
@@ -10,12 +10,13 @@ import (
 type Product struct {
 	Id uuid.UUID
 	Url string
+	Title string
 	Store string
 	Last_checked_at time.Time
 	Created_at time.Time
 }
 
 func (p Product) String() string {
-		return fmt.Sprintf("DEBUG Product: %s \nUrl: %s \nStore: %s \nLast Checked: %s \nCreated At: %s\n",
-							p.Id, p.Url, p.Store, p.Last_checked_at, p.Created_at)
+		return fmt.Sprintf("DEBUG Product: %s \nUrl: %s \nTitle: %s \nStore: %s \nLast Checked: %s \nCreated At: %s\n",
+							p.Id, p.Url, p.Title, p.Store, p.Last_checked_at, p.Created_at)
 }
