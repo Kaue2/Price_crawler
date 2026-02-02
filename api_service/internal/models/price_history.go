b21df596e25633d7b0b98dce07db4c81a399fa26
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-02 13:59:41 -0300

    refac: tornar campos das structs públicos

--- api_service/internal/models/price_history.go
+++ api_service/internal/models/price_history.go
@@ -1,14 +1,21 @@
 package models
 
 import (
+	"fmt"
 	"time"
+
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
 )
 
 type price_chapter struct {
-	id uuid.UUID
-	product uuid.UUID
-	value decimal.Decimal
-	created_at time.Time
+	Id uuid.UUID
+	Product uuid.UUID
+	Value decimal.Decimal
+	Created_at time.Time
+}
+
+func (p price_chapter) String() string {
+	return fmt.Sprintf("DEBUG: Price Chapter: %s: \nProduct: %s \nValue: %v \nCreated At: %s\n",
+					p.Id, p.Product, p.Value, p.Created_at)
 }
--- api_service/internal/models/product.go
+++ api_service/internal/models/product.go
@@ -1,15 +1,21 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Product struct {
-	id uuid.UUID
-	url string
-	store string
-	last_checked_at time.Time
-	created_at time.Time
+	Id uuid.UUID
+	Url string
+	Store string
+	Last_checked_at time.Time
+	Created_at time.Time
+}
+
+func (p Product) String() string {
+		return fmt.Sprintf("DEBUG Product: %s \nUrl: %s \nStore: %s \nLast Checked: %s \nCreated At: %s\n",
+							p.Id, p.Url, p.Store, p.Last_checked_at, p.Created_at)
 }
