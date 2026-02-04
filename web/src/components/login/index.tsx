cd9a88e7f0ae4354bf1ccbaef3b7667c2383820c
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-04 16:20:33 -0300

    add: adicionar módulo para componentes

---
+++ web/src/components/login/index.tsx
@@ -0,0 +1,11 @@
+
+function Login() {
+  return(
+    <>
+      <input type="text" placeholder="Digite seu email" />
+      <input type="text" placeholder="Digite sua senha" />
+    </>
+  )
+}
+
+export default Login
