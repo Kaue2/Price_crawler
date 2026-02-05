ab7464ce96e50b64fffbcfa56c44da2dd59240ee
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-05 19:43:33 -0300

    feat: adicionar módulos dos componentes

--- web/src/components/login/index.tsx
+++ web/src/components/login/index.tsx
@@ -1,11 +1,31 @@
 
-function Login() {
+export function AppTitle() {
   return(
     <>
-      <input type="text" placeholder="Digite seu email" />
-      <input type="text" placeholder="Digite sua senha" />
+      <h1 className="text-white text-5xl">Price Crawler</h1>
     </>
   )
 }
 
-export default Login
+interface SwitchSignsProps {
+  option: "signIn" | "signUp";
+  setOption: (val: "signIn" | "signUp") => void; // função recebida do pai
+}
+
+export function SwitchSigns({option, setOption}: SwitchSignsProps) {
+  return(
+    <div className="flex gap-2">
+      <button onClick={() => setOption("signIn")}>
+        <h3 className={`${option == "signIn" ? "text-white" : "text-[#545454]"}`}>
+          Sign In
+        </h3>
+      </button>
+      <h3 className="text-white">|</h3>
+      <button onClick={() => setOption("signUp")}>
+        <h3 className={`${option == "signUp" ? "text-white" : "text-[#545454]"}`}>
+          Sign Up
+        </h3>
+      </button>
+    </div>
+  )
+}
