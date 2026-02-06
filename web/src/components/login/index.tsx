aaca01c082c4402d48df65f2ea38ef440576eea9
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-06 16:15:48 -0300

    feat: criar componentes para tela de login

--- web/src/components/login/index.tsx
+++ web/src/components/login/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 
 export function AppTitle() {
   return(
@@ -27,5 +28,58 @@ export function SwitchSigns({option, setOption}: SwitchSignsProps) {
         </h3>
       </button>
     </div>
+  )
+}
+
+interface InputOptions{
+  text: string;
+  type: "text" | "password";
+  action: (val: string) => void;
+}
+
+export function InputForm({ text, type, action }: InputOptions) {
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    action(inputValue)
+  }, [inputValue, action]);
+
+  return(
+    <>
+      <input 
+        className="border border-black px-3 py-4 rounded-md w-full" 
+        type={type} placeholder={text}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </>
+  )
+}
+
+interface FormProps {
+  children: React.ReactNode
+}
+
+export function Form({ children }: FormProps){
+  return(
+    <div className="flex flex-col items-center gap-8 rounded-md
+    bg-[#F8F8F8] px-5 py-12 ">
+      {children}      
+    </div>
+  )
+}
+
+interface BtnFormProps{
+  text: string;
+  action: () => void;
+}
+
+export function BtnForm({ text, action }: BtnFormProps){
+  return(
+    <>
+      <button onClick={() =>  action()}
+        className="bg-black text-white rounded-md
+        py-4 w-full text-center">{text}</button>
+    </>
   )
 }
