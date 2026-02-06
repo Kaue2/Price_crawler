9e8acf19da69bd2f886b34aefe49654bdd2713ee
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-06 16:42:28 -0300

    refac: exportar struct price chapter

--- api_service/internal/models/price_history.go
+++ api_service/internal/models/price_history.go
@@ -8,14 +8,14 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type price_chapter struct {
+type Price_chapter struct {
 	Id uuid.UUID
 	Product uuid.UUID
 	Value decimal.Decimal
 	Created_at time.Time
 }
 
-func (p price_chapter) String() string {
+func (p Price_chapter) String() string {
 	return fmt.Sprintf("DEBUG: Price Chapter: %s: \nProduct: %s \nValue: %v \nCreated At: %s\n",
 					p.Id, p.Product, p.Value, p.Created_at)
 }
