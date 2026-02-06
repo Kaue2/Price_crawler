78b85b208efd77be36591d2378f14aa800f09269
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-06 16:16:27 -0300

    feat: implementar tela de login

--- web/src/pages/login/loginPage.tsx
+++ web/src/pages/login/loginPage.tsx
@@ -1,13 +1,30 @@
-import { AppTitle, SwitchSigns } from "../../components/login";
+import { AppTitle, SwitchSigns, InputForm, Form, BtnForm } from "../../components/login";
 import { useState } from "react";
 
 function Login() {
   const [activeTab, setActiveTab] = useState<"signIn" | "signUp">("signIn");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  function ConfirmLogin() {
+    if (userEmail == "") return;
+    if (userPassword == "") return;
+
+    console.log(userEmail);
+    console.log(userPassword);
+  }
+
 
   return(
-    <div className="flex flex-col items-center gap-12">
+    <div className="flex flex-col items-center gap-12 py-6">
       <AppTitle />
       <SwitchSigns option={activeTab} setOption={setActiveTab}/>
+      <Form>
+        <h2 className="text-3xl">Bem vindo de volta!</h2>
+        <InputForm text="exemplo@gmail.com" type="text" action={setUserEmail} />
+        <InputForm text="password" type="password" action={setUserPassword} />
+        <BtnForm text="Sign In" action={ConfirmLogin} />
+      </Form>
     </div>
   )
 }
