c72ca11b3ff009fdb890506edaea7ec1e5859dd9
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-09 15:15:59 -0300

    add: adicionar endpoint e função para create user

--- api_service/internal/api/handler.go
+++ api_service/internal/api/handler.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"price-crawler-api/internal/services"
 	"price-crawler-api/internal/services/database"
 	"price-crawler-api/internal/services/queue"
 )
@@ -10,10 +12,11 @@ import (
 type Handler struct {
 	store *database.Store
 	rabbit *queue.RabbitMQ
+	userService *services.UserService
 }
 
-func NewHandler(s *database.Store, r *queue.RabbitMQ) *Handler {
-	return &Handler{store: s, rabbit: r}
+func NewHandler(s *database.Store, r *queue.RabbitMQ, u *services.UserService) *Handler {
+	return &Handler{store: s, rabbit: r, userService: u}
 }
 
 func (h *Handler) GetAllProduct(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +29,23 @@ func (h *Handler) GetAllProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(products)
+}
+
+func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var body services.UserServiceRequestBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "Erro: corpo da requisição não suportado", http.StatusBadRequest)
+		return
+	}
+
+	err := h.userService.Create(body.Email, body.PasswordPlain)
+	if err != nil {
+		log.Printf("ERROR: erro ao criar usuário: %s", err)
+		http.Error(w, "Erro: não foi possível criar usuário", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w)
 }
--- api_service/internal/api/routes.go
+++ api_service/internal/api/routes.go
@@ -8,9 +8,19 @@ func (h *Handler) RegisterRoutes() *http.ServeMux {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			http.Error(w, "ERRO: método não permitido", http.StatusMethodNotAllowed)
+			return 
 		}
 
 		h.GetAllProduct(w, r)
+	})
+
+	mux.HandleFunc("/create-user", func (w http.ResponseWriter, r *http.Request)  {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Erro: método não permitido", http.StatusMethodNotAllowed)
+			return
+		}
+
+		h.CreateUser(w, r)
 	})
 
 	return mux
