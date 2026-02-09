c1cc1bea43b346eb102983468c15c2a011788eaa
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-09 15:39:27 -0300

    refac: alterar routes para dar suporte ao nova arquitetura de handler

--- api_service/internal/api/routes.go
+++ api_service/internal/api/routes.go
@@ -2,26 +2,10 @@ package api
 
 import "net/http"
 
-func (h *Handler) RegisterRoutes() *http.ServeMux {
+func RegisterRoutes(UserHandler *UserHandler) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "ERRO: método não permitido", http.StatusMethodNotAllowed)
-			return 
-		}
-
-		h.GetAllProduct(w, r)
-	})
-
-	mux.HandleFunc("/create-user", func (w http.ResponseWriter, r *http.Request)  {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Erro: método não permitido", http.StatusMethodNotAllowed)
-			return
-		}
-
-		h.CreateUser(w, r)
-	})
+	mux.HandleFunc("POST /create-user", UserHandler.CreateUser)
 
 	return mux
 }
