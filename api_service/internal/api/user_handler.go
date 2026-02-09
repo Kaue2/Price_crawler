198d4493feb3ff20b33fd4a2657c156d5cefc02d
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-09 15:38:19 -0300

    add: desaclopar user handler do hanlder genérico

---
+++ api_service/internal/api/user_handler.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"price-crawler-api/internal/services"
+)
+
+type UserHandler struct {
+	userService *services.UserService
+}
+
+func NewUserHandler(s *services.UserService) *UserHandler{
+	return &UserHandler{userService: s}
+}
+
+func (u *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var body services.UserServiceRequestBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "Erro: corpo da requisição não suportado", http.StatusBadRequest)
+		return
+	}
+
+	err := u.userService.Create(body.Email, body.PasswordPlain)
+	if err != nil {
+		log.Printf("ERROR: erro ao criar usuário: %s", err)
+		http.Error(w, "Erro: não foi possível criar usuário", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w)
+}
