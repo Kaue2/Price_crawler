224783c8a310bbae4d9e30605ebf6a6c9b85faf7
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-24 16:12:17 -0300

    add: registrar rota para login de usuários

--- api_service/internal/api/routes.go
+++ api_service/internal/api/routes.go
@@ -5,7 +5,9 @@ import "net/http"
 func RegisterRoutes(UserHandler *UserHandler) *http.ServeMux {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("POST /login-user", UserHandler.LoginUser)
 	mux.HandleFunc("POST /create-user", UserHandler.CreateUser)
+
 
 	return mux
 }
